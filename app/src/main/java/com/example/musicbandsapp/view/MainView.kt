b3b6902d56580e2ddcaef504b14b7399b14c1ce6
BartoszJab <bartekj1221@wp.pl>
2022-10-23 16:37:29 +0200

    Update MainView

--- app/src/main/java/com/example/musicbandsapp/view/MainView.kt
+++ app/src/main/java/com/example/musicbandsapp/view/MainView.kt
@@ -1,17 +1,15 @@
 package com.example.musicbandsapp.view
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.musicbandsapp.ui.theme.MusicBandsAppTheme
+import com.example.musicbandsapp.view.composable.RowItem
 import com.example.musicbandsapp.viewmodel.BandsViewModel
 import org.koin.androidx.compose.getViewModel
 
@@ -19,9 +17,13 @@ import org.koin.androidx.compose.getViewModel
 fun MainView(bandsViewModel: BandsViewModel = getViewModel()) {
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(items = bandsViewModel.bands, key = { it.id }) {
-            Row(modifier = Modifier.padding(16.dp)) {
-                Text(it.name)
-            }
+            RowItem(
+                band = it,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable { }
+            )
+
             Divider()
         }
     }
---
+++ app/src/main/java/com/example/musicbandsapp/view/composable/RowItem.kt
@@ -0,0 +1,64 @@
+package com.example.musicbandsapp.view.composable
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.musicbandsapp.model.Band
+import com.example.musicbandsapp.ui.theme.MusicBandsAppTheme
+
+@Composable
+fun RowItem(band: Band, modifier: Modifier = Modifier) {
+    Box(modifier = modifier) {
+        Row(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            AsyncImage(
+                model = band.logoImage ?: band.bandImage,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(74.dp)
+                    .clip(RoundedCornerShape(percent = 50)),
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    band.name,
+                    style = TextStyle(
+                        fontWeight = FontWeight.W300,
+                        fontSize = 24.sp
+                    )
+                )
+
+                Text(
+                    band.genre.toString(),
+                    style = MaterialTheme.typography.subtitle2,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RowItemPreview() {
+    MusicBandsAppTheme {
+
+    }
+}
