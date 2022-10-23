629a97a0a992ea0bd3d49bd1fd5f3bc2480facda
BartoszJab <bartekj1221@wp.pl>
2022-10-23 15:17:21 +0200

    Set MainView as default view and set up koin

--- app/src/main/java/com/example/musicbandsapp/MainActivity.kt
+++ app/src/main/java/com/example/musicbandsapp/MainActivity.kt
@@ -3,41 +3,28 @@ package com.example.musicbandsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.musicbandsapp.di.appModule
 import com.example.musicbandsapp.ui.theme.MusicBandsAppTheme
+import com.example.musicbandsapp.view.MainView
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MainActivity)
+            modules(appModule)
+        }
+
         setContent {
             MusicBandsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainView()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MusicBandsAppTheme {
-        Greeting("Android")
     }
 }
