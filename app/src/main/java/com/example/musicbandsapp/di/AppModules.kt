2502cb295dc2c4f56e6b0e1acc386c5d92931b67
BartoszJab <bartekj1221@wp.pl>
2022-10-23 14:49:51 +0200

    Add koin dependency injection

---
+++ app/src/main/java/com/example/musicbandsapp/di/AppModules.kt
@@ -0,0 +1,22 @@
+package com.example.musicbandsapp.di
+
+import com.example.musicbandsapp.api.BandsService
+import com.example.musicbandsapp.viewmodel.BandsViewModel
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single {
+        BandsService(client = HttpClient(Android) {
+            install(Logging)
+            install(ContentNegotiation) { json() }
+        })
+    }
+
+    viewModel { BandsViewModel(get()) }
+}
