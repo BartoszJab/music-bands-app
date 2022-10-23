f513189688e2f50132aab3300e05477e5391f2d5
BartoszJab <bartekj1221@wp.pl>
2022-10-23 19:41:57 +0200

    Update DetailsView

--- app/src/main/java/com/example/musicbandsapp/view/DetailsView.kt
+++ app/src/main/java/com/example/musicbandsapp/view/DetailsView.kt
@@ -1,32 +1,63 @@
 package com.example.musicbandsapp.view
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.musicbandsapp.model.Band
 import com.example.musicbandsapp.viewmodel.BandsViewModel
-import org.koin.androidx.compose.getViewModel
 
 @Composable
-fun DetailsView(itemId: Long, bandsViewModel: BandsViewModel = getViewModel()) {
-    val band: Band? = bandsViewModel.bands.find { it.id == itemId }
+fun DetailsView(itemId: Long, bandsViewModel: BandsViewModel) {
 
-    band?.let {
-        Column(modifier = Modifier.fillMaxSize()) {
+    val selectedBand: Band? = bandsViewModel.bands.find { it.id == itemId }
+
+    selectedBand?.let { band ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
             AsyncImage(
-                model = it.bandImage,
+                model = band.bandImage,
                 contentDescription = "Band image",
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight(0.4f)
             )
+            Text("Members", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
+            Text(band.members.joinToString(", "))
 
-            Text(it.albums.toString())
-            Text(it.members.toString())
+            Spacer(Modifier.height(8.dp))
+
+            Text("Albums", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
+            LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+                items(band.albums) {
+                    Box(modifier = Modifier.border(1.dp, Color.Black)) {
+                        Column(modifier = Modifier.padding(8.dp)) {
+                            Text(it.name)
+                            Text(it.releaseDate)
+                        }
+                    }
+                }
+            }
+
+            Text("Reference", style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold))
+            Column(Modifier.fillMaxWidth()) {
+                Text(band.reference.spotify ?: "")
+                Text(band.reference.appleMusic ?: "")
+                Text(band.reference.youtube ?: "")
+            }
         }
     } ?: run {
         Column(
